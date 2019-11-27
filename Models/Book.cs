86e1e3a8c7e8b0f4b90348bf7a7d5ad40e6989d9
Nnaji Victor <nnajivictor0@gmail.com>
2019-11-27 14:19:56 +0100

    add model classes

---
+++ Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string  ISBN { get; set; }
+        public string Title { get; set; }
+        public DateTime DatePublished { get; set; }
+
+    }
+}
