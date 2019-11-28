400972b779a26b9b3bbb9a54b0672a85964b4688
Nnaji Victor <nnajivictor0@gmail.com>
2019-11-28 01:09:17 +0100

    add modelbuilder entity for many to many relationship for the bookcategory

--- Services/BookDbContext.cs
+++ Services/BookDbContext.cs
@@ -1,11 +1,43 @@
-﻿using System;
+﻿using BookApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BookApi.Services
 {
-    public class BookDbContext
+    public class BookDbContext : DbContext
     {
+        public BookDbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<Book> Book { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+        public DbSet<BookCategory> BookCategories { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BookCategory>()
+                .HasKey(bc => new { bc.BookId, bc.Category });
+
+            modelBuilder.Entity<BookCategory>()
+                .HasOne(b => b.Book)
+                .WithMany(bc => bc.BookCategories)
+                .HasForeignKey(b => b.BookId);
+
+            modelBuilder.Entity<BookCategory>()
+                .HasOne(c => c.Category)
+                .WithMany(bc => bc.BookCategories)
+                .HasForeignKey(c => c.CategoryId);
+        }
     }
 }
