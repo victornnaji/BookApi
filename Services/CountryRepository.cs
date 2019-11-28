154efc44be0a61908b691bd6064aa22c42cc58e7
Nnaji Victor <nnajivictor0@gmail.com>
2019-11-28 15:37:11 +0100

    implement getcountry and getcountry by id

---
+++ Services/CountryRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookApi.Models;
+
+namespace BookApi.Services
+{
+    public class CountryRepository : IcountryRepository
+    {
+        private BookDbContext _countryContext;
+
+        public CountryRepository(BookDbContext countryContext)
+        {
+            _countryContext = countryContext;
+        }
+        public ICollection<Author> GetAuthorsFromACountry(int countryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Country> GetCountries()
+        {
+            return _countryContext.Countries.OrderBy(c => c.Name).ToList();
+        }
+
+        public Country GetCountry(int countryId)
+        {
+            return _countryContext.Countries.Where(c => c.Id == countryId).FirstOrDefault();
+        }
+
+        public Country GetCountryOfAuthor(int authorId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
