c8a9783ccc5531b157d1aa9543d2c58a3f73e4d3
Nnaji Victor <nnajivictor0@gmail.com>
2019-11-30 22:32:57 +0100

    added get all reviews controller

---
+++ Controllers/ReviewersController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookApi.Dtos;
+using BookApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewersController : Controller
+    {
+        private IReviewerRepository _reviewerRepository;
+
+        public ReviewersController(IReviewerRepository reviewerRepository)
+        {
+            _reviewerRepository = reviewerRepository;
+        }
+
+        // GET: api/Reviewers
+        [HttpGet]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200, Type= typeof(ReviewerDto))]
+        public IActionResult GetReviewers()
+        {
+            var reviewers = _reviewerRepository.GetReviewers();
+
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var reviewerDto = new List<ReviewerDto>();
+
+            foreach(var reviewer in reviewers)
+            {
+                reviewerDto.Add(new ReviewerDto
+                {
+                    Id = reviewer.Id,
+                    FirstName = reviewer.FirstName,
+                    LastName = reviewer.LastName
+                });
+            }
+
+            return Ok(reviewerDto);
+        }
+
+       
+    }
+}
