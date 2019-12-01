014e2930d597d418e94499824c506391b4b8324a
Nnaji Victor <nnajivictor0@gmail.com>
2019-12-01 14:46:54 +0100

    add get reviews of a book controller

--- Controllers/ReviewsController.cs
+++ Controllers/ReviewsController.cs
@@ -13,10 +13,15 @@ namespace BookApi.Controllers
     [ApiController]
     public class ReviewsController : ControllerBase
     {
+        private IReviewerRepository _reviewerRepository;
         private IReviewRepository _reviewRepository;
-        public ReviewsController(IReviewRepository reviewRepository)
+        private IBookRepository _bookRepository;
+
+        public ReviewsController(IReviewerRepository reviewerRepository, IReviewRepository reviewRepository, IBookRepository bookRepository)
         {
+            _reviewerRepository = reviewerRepository;
             _reviewRepository = reviewRepository;
+            _bookRepository = bookRepository;
         }
 
         // GET: api/Reviews
@@ -73,6 +78,35 @@ namespace BookApi.Controllers
             var bookDto = new BookDto { Id = book.Id, Isbn = book.Isbn, Title = book.Title, DatePublished = book.DatePublished };
 
             return Ok(bookDto);
+        }
+
+        //api/reviews/books/bookId
+        [HttpGet("books/{bookId}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<ReviewDto>))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult GetReviewsForABook(int bookId)
+        {
+            if (!_bookRepository.BookExists(bookId))
+                return NotFound();
+
+            var reviews = _reviewRepository.GetReviewsOfABook(bookId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var reviewsDto = new List<ReviewDto>();
+            foreach (var review in reviews)
+            {
+                reviewsDto.Add(new ReviewDto()
+                {
+                    Id = review.Id,
+                    Headline = review.Headline,
+                    ReviewText = review.ReviewText
+                });
+            }
+
+            return Ok(reviewsDto);
         }
 
 
