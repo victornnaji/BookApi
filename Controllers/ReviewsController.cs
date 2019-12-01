0556f4e75b615f19c93d598fac79ed6e49e1aaa6
Nnaji Victor <nnajivictor0@gmail.com>
2019-12-01 14:43:44 +0100

    added get review with an id controller

--- Controllers/ReviewsController.cs
+++ Controllers/ReviewsController.cs
@@ -39,6 +39,42 @@ namespace BookApi.Controllers
             return Ok(reviewDto);
         }
 
-        
+        //GET: api/reviews/reviewId
+        [HttpGet("{reviewId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(ReviewDto))]
+        public IActionResult GetReview(int reviewId)
+        {
+            if (!_reviewRepository.ReviewExists(reviewId)) return NotFound();
+
+            var review = _reviewRepository.GetReview(reviewId);
+
+            if (!ModelState.IsValid) return BadRequest();
+
+            var reviewDto = new ReviewDto { Id = review.Id, Headline = review.Headline, ReviewText = review.ReviewText };
+
+            return Ok(reviewDto);
+        }
+
+        //GET: api/reviews/reviewId/book
+        [HttpGet("{reviewId}/book")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(BookDto))]
+        public IActionResult GetBookOfAReview(int reviewId)
+        {
+            if (!_reviewRepository.ReviewExists(reviewId)) return NotFound();
+
+            var book = _reviewRepository.GetBookOfAReview(reviewId);
+
+            if (!ModelState.IsValid) return BadRequest();
+
+            var bookDto = new BookDto { Id = book.Id, Isbn = book.Isbn, Title = book.Title, DatePublished = book.DatePublished };
+
+            return Ok(bookDto);
+        }
+
+
     }
 }
